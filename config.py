1164f7f830d548581eda5d4637efff688e362037
VILLAIN MUSIC <ramnik5911@gmail.com>
2025-07-03 08:21:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ixmusic/VILLAIN_MUSIC",
+    "https://github.com/ixasta077/VILLAIN_MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
