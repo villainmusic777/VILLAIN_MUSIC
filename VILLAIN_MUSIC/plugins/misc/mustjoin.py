503a46b55dc105c08abcbd732d388b9c57d76488
ixasta077 <astak1861@gmail.com>
2025-07-06 21:00:15 +0530

    Update mustjoin.py

--- VILLAIN_MUSIC/plugins/misc/mustjoin.py
+++ VILLAIN_MUSIC/plugins/misc/mustjoin.py
@@ -5,7 +5,7 @@ from VILLAIN_MUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "iamvillain77"
+MUST_JOIN = "ixasta1"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
@@ -16,7 +16,7 @@ async def must_join_channel(app: Client, msg: Message):
             await app.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
-                link = "https://t.me/iamvillain77" + MUST_JOIN
+                link = "https://t.me/ixasta1" + MUST_JOIN
             else:
                 chat_info = await app.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
@@ -29,7 +29,7 @@ async def must_join_channel(app: Client, msg: Message):
                                 InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/oldskoolgc"),
                             ],
                             [
-                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/iamvillain77"),
+                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/ixasta1"),
                             ]
                         ]
                     )
