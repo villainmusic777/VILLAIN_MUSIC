76687a148606e40bbea9a26f8206775aff728df6
villainmusic777 <villainmusicgithub@gmail.com>
2025-07-13 09:53:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/astamusic/ASTA_MUSIC_ARYAN",
+    "https://github.com/villainmusic777/VILLAIN_MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
